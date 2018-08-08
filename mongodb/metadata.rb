c5faeb013de32c9e5df47cb4e998a5ee7befabd6
Dzuy Tran <dzuyt@perfectomobile.com>
2018-08-07 22:05:45 -0400

    Update mongodb config version.

--- mongodb/metadata.rb
+++ mongodb/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dzuyt@perfectomobile.com'
 license 'All Rights Reserved'
 description 'Installs/Configures mongodb'
 long_description 'Installs/Configures mongodb'
-version '0.1.0'
+version '0.2.0'
 
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
